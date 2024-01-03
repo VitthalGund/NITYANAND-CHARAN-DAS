2daa347bf052c7af7311748f4ab96cc6845dfc1d
VitthalGund <vitthalgund30@gmail.com>
2024-01-03 17:41:49 +0530

    responsive added in title and name

--- src/app/Components/LandingPhoto.tsx
+++ src/app/Components/LandingPhoto.tsx
@@ -5,14 +5,14 @@ const LandingPhoto = ({ name, title }: { name: string, title: string }) => {
     return (
         <>
             <div className="relative w-full h-[20rem] mx-auto overflow-y-hidden object-cover">
-                <Image src={"/Nityanand Charan Das.avif"}
+                <Image src={"/Nityanand Charan Das.jpeg"}
                     width={30} height={10} alt='titleImage' content='.png'
                     className='w-full h-[20rem] bg-black'
                 />
                 <div className="absolute inset-0 bg-gradient-to-tr from-gray-900 via-gray-900/40"></div>
                 <div className="absolute inset-0 flex flex-col justify-end items-start m-10 flex-wrap">
-                    <h2 className="text-white sm:text-lg lg:text-1xl font-bold">{name}</h2>
-                    <h2 className="text-white font-bold text-2xl sm:text-2xl md:w-[32rem] lg:text-4xl sm:w-80 ">{title}</h2>
+                    <h2 id='name' className="text-white sm:text-lg lg:text-1xl font-bold">{name}</h2>
+                    <h2 id='title' className="text-white font-bold text-2xl sm:text-2xl md:w-[32rem] lg:text-4xl sm:w-80 ">{title}</h2>
                 </div>
             </div>
         </>
