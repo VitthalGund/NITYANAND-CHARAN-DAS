cc189b6a6fab0682198c952c4ab5392c9b52fdc6
VitthalGund <vitthalgund30@gmail.com>
2024-01-03 16:57:11 +0530

    Instructor component added

---
+++ src/app/Components/Instructor.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link';
+import Course from './Course';
+
+
+const Instructor = () => {
+    return (
+        <div>
+            <div className='mb-6' id='INSTRUCTOR'>
+                <h2 className='text-2xl sm:text-left text-center font-bold mx-10 m-5 p-3 lg:text-4xl'>About the Instructor</h2>
+                <div className='flex flex-col justify-center items-center lg:grid grid-flow-col grid-cols-3'>
+                    <Image src="/icon.jpeg" alt=""
+                        width={100} height={100}
+                        className='rounded-full lg:ml-[7rem] w-56 lg:self-end'
+                    />
+                    <p className='p-3 m-2 lg:m-0 lg:p-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate temporibus illo repellendus officiis sunt pariatur nesciunt iusto similique? Accusantium a omnis aliquam quisquam, ab amet?</p>
+                    <p className='p-3 m-2 lg:m-0 lg:p-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate temporibus illo repellendus officiis sunt pariatur nesciunt iusto similique? Accusantium a omnis aliquam quisquam, ab amet?</p>
+                </div>
+                <div className='flex flex-col justify-center items-center sm:flex-row p-5 flex-wrap'>
+                    <Link href={"https://www.facebook.com/NityanandCharanD/"}
+                        target='_blank'
+                        className="facebook flex flex-row justify-center items-center text-xl lg:mr-20 mt-2 mb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 48 48">
+                            <linearGradient id="Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1" x1="9.993" x2="40.615" y1="9.993" y2="40.615" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#2aa4f4"></stop><stop offset="1" stopColor="#007ad9"></stop></linearGradient><path fill="url(#Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1)" d="M24,4C12.954,4,4,12.954,4,24s8.954,20,20,20s20-8.954,20-20S35.046,4,24,4z"></path><path fill="#fff" d="M26.707,29.301h5.176l0.813-5.258h-5.989v-2.874c0-2.184,0.714-4.121,2.757-4.121h3.283V12.46 c-0.577-0.078-1.797-0.248-4.102-0.248c-4.814,0-7.636,2.542-7.636,8.334v3.498H16.06v5.258h4.948v14.452 C21.988,43.9,22.981,44,24,44c0.921,0,1.82-0.084,2.707-0.204V29.301z"></path>
+                        </svg>
+                        Facebook
+                    </Link>
+                    <Link
+                        href={"https://twitter.com/nityananda_c?lang=en"}
+                        target='_blank'
+                        className="x flex flex-row justify-center items-center text-xl lg:mr-20 mt-2 mb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="36" height="36" viewBox="0 0 50 50">
+                            <path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"></path>
+                        </svg>
+                        Twitter
+                    </Link>
+                    <Link
+                        href={"https://www.youtube.com/c/NityanandCharanDas"}
+                        target='_blank'
+                        className="youtube flex flex-row justify-center items-center text-xl lg:mr-20 mt-2 mb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 48 48">
+                            <path fill="#FF3D00" d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"></path><path fill="#FFF" d="M20 31L20 17 32 24z"></path>
+                        </svg>
+                        Youtube
+                    </Link>
+                    <Link
+                        href={"https://www.instagram.com/nityanand_charan_das/"}
+                        target='_blank'
+                        className="instagram flex flex-row justify-center items-center text-xl lg:mr-20 mt-2 mb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 48 48">
+                            <radialGradient id="yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1" cx="19.38" cy="42.035" r="44.899" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#fd5"></stop><stop offset=".328" stopColor="#ff543f"></stop><stop offset=".348" stopColor="#fc5245"></stop><stop offset=".504" stopColor="#e64771"></stop><stop offset=".643" stopColor="#d53e91"></stop><stop offset=".761" stopColor="#cc39a4"></stop><stop offset=".841" stopColor="#c837ab"></stop></radialGradient><path fill="url(#yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1)" d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"></path><radialGradient id="yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2" cx="11.786" cy="5.54" r="29.813" gradientTransform="matrix(1 0 0 .6663 0 1.849)" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#4168c9"></stop><stop offset=".999" stopColor="#4168c9" stopOpacity="0"></stop></radialGradient><path fill="url(#yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2)" d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"></path><path fill="#fff" d="M24,31c-3.859,0-7-3.14-7-7s3.141-7,7-7s7,3.14,7,7S27.859,31,24,31z M24,19c-2.757,0-5,2.243-5,5	s2.243,5,5,5s5-2.243,5-5S26.757,19,24,19z"></path><circle cx="31.5" cy="16.5" r="1.5" fill="#fff"></circle><path fill="#fff" d="M30,37H18c-3.859,0-7-3.14-7-7V18c0-3.86,3.141-7,7-7h12c3.859,0,7,3.14,7,7v12	C37,33.86,33.859,37,30,37z M18,13c-2.757,0-5,2.243-5,5v12c0,2.757,2.243,5,5,5h12c2.757,0,5-2.243,5-5V18c0-2.757-2.243-5-5-5H18z"></path>
+                        </svg>
+                        Instagram
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Instructor
